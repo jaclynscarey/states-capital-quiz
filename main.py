9b389658448e010196999556c36e0fdc934d141a
Jaclyn Carey <jsoyama@gmail.com>
2023-05-25 22:52:52 -0400

    add total score and display running tally

--- main.py
+++ main.py
@@ -12,18 +12,22 @@ play_again = 'yes'
 
 while play_again == 'yes':
     random.shuffle(states)
+    score = 0
 
-    for state in states:
-        answer = input("What is the capital of " + state["name"] + "?: ").lower()
+    for index, state in enumerate(states, 1):
+        answer = input(f"{index}) What is the capital of " + state["name"] + "?: ").lower()
         if answer == state["capital"].lower():
             print("\nThat's CORRECT!\n")
             state["correct"] += 1
+            score += 1
             total = state["correct"] + state["wrong"]
-            print(f"You got the right answer {state['correct']} out of {total} tries.\n")
+            print(f"You got this right {state['correct']} out of {total} tries.\n\nYour current score is {score} out of 50.\n")
+
+
         else:
             print(f"\nThat's INCORRECT! The capital of {state['name']} is {state['capital']}.\n")
             state["wrong"] += 1
             total = state["correct"] + state["wrong"]
-            print(f"You got the right answer {state['correct']} out of {total} tries.\n")
+            print(f"You got the right answer {state['correct']} out of {total} tries.\n\nYour current score is {score} out of 50.\n")
 
     play_again = input("Would you like to play again? (yes/no): \n").lower()
